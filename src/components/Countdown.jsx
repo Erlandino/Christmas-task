5e0ccec85c23a68b01fdf7cdeb0763ae8c9de9e6
Erlandino <erlend.kodehode@gmail.com>
2022-12-22 11:23:15 +0100

    added fuctionality to countdown and posting in greetings

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,15 +1,56 @@
+import { useEffect, useState } from "react";
+
 export default function Countdown() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  const year = currentTime.getFullYear();
+
+  const endOfYearTime = new Date(`${year}, 12, 23, 23:59:59`);
+
+  function getTime(type) {
+    const seconds = endOfYearTime.getSeconds() - currentTime.getSeconds();
+    const hours = endOfYearTime.getHours() - currentTime.getHours();
+    const days = Math.floor((endOfYearTime - currentTime) / 86400000);
+
+    if (type === "seconds") return seconds;
+    if (type === "hours") return hours;
+    if (type === "days" && days >= 0) return days;
+    if (type === "days" && days < 0) return 365 + days;
+  }
+
+  const [timeLeft, setTimeLeft] = useState({
+    seconds: getTime("seconds"),
+    hours: getTime("hours"),
+    days: getTime("days"),
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+      setTimeLeft((prevTime) => {
+        return {
+          seconds: getTime("seconds"),
+          hours: getTime("hours"),
+          days: getTime("days"),
+        };
+      });
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <div className="countdown">
       <h1 className="countdown-title">JULE NEDTELLING</h1>
       <p>Dager</p>
-      <p></p>
+      <p>{timeLeft.days}</p>
 
       <p>Timer</p>
-      <p></p>
+      <p>{timeLeft.hours}</p>
 
       <p>Sekunder</p>
-      <p></p>
+      <p>{timeLeft.seconds}</p>
     </div>
   );
 }
--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,34 +1,84 @@
+import { useEffect, useState } from "react";
+
 export default function Greetings() {
-  const otherGreetings = [
+  const [formItem, setFormItem] = useState({
+    name: "",
+    message: "",
+  });
+
+  const [otherGreetings, setOtherGreetings] = useState([
     {
       name: "Hans",
       message:
         "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+      dateSent: "12-12-2022",
     },
     {
       name: "Olav",
       message:
         "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. readable content of a page when looking at its layout.",
+      dateSent: "13-12-2022",
     },
     {
       name: "Inger",
       message: "It is a long established fact that a reader will be distracted by",
+      dateSent: "17-12-2022",
     },
-  ];
+  ]);
 
+  function inputValue(event) {
+    setFormItem((prevItem) => {
+      return {
+        name: event.target.id === "name" ? event.target.value : prevItem.name,
+        message: event.target.id === "message" ? event.target.value : prevItem.message,
+      };
+    });
+  }
+  function addGreeting(event) {
+    event.preventDefault();
+    localStorage.setItem("Greeting", JSON.stringify(formItem));
+    postGreeting();
+  }
+
+  useEffect(() => {
+    postGreeting();
+  }, []);
+
+  function postGreeting() {
+    const post = localStorage.getItem("Greeting");
+    console.log(post);
+    if (post) {
+      setOtherGreetings((prevState) => {
+        return [JSON.parse(post), ...prevState];
+      });
+    }
+  }
+
+  // if (localStorage.getItem("Greeting")) console.log("yes");
   return (
     <div className="greetings">
-      <form className="greetings__form" action="">
+      <form className="greetings__form">
         <h1 className="greetings__form__title">SEND JULEHILSE</h1>
-        <label className="greetings__form__label" for="name">
+        <label className="greetings__form__label" htmlFor="name">
           Navn
         </label>
-        <input className="greetings__form__input" type="text" id="name" />
-        <label className="greetings__form__label" for="message">
+        <input
+          className="greetings__form__input greetings__form__input-name"
+          type="text"
+          id="name"
+          onChange={inputValue}
+        />
+        <label className="greetings__form__label" htmlFor="message">
           Julehilsen
         </label>
-        <textarea className="greetings__form__input" id="message"></textarea>
-        <input className="greetings__form__submit" type="submit" />
+        <textarea className="greetings__form__input" id="message" onChange={inputValue}></textarea>
+        <input
+          className="greetings__form__submit"
+          type="submit"
+          onClick={addGreeting}
+          value="Send"
+          onChange={() => inputValue("message")}
+        />
       </form>
 
       <div className="greetings__list">
