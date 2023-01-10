93ccc66de71e5e88af13e198bd910ed0d44241d3
Erlandino <erlend.kodehode@gmail.com>
2023-01-10 14:13:12 +0100

    added comments to all components and styled button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,26 +7,33 @@ export default function Footer() {
   return (
     // Footer container
     <footer className="footer">
-      {/* list */}
+      {/* List */}
       <ul className="footer__list">
+        {/* List item */}
         <li className="footer__list__item">
+          {/* Link to github repository */}
           <a
             className="footer__list__item__link"
             href="https://github.com/Erlandino/Christmas-task"
             target="_blank"
             rel="noreferrer"
           >
+            {/* Image of github logo */}
             <img className="footer__list__item__link__logo" src={Github} alt="" /> Github
           </a>
         </li>
+        {/* List item */}
         <li className="footer__list__item">|</li>
+        {/* List item */}
         <li className="footer__list__item">
+          {/* Link to figma file */}
           <a
             className="footer__list__item__link"
             href="https://www.figma.com/file/cPIkeXKbLLojYG7EyJXObd/ChristmasCalender?node-id=2%3A33&t=voO52qHem3ftnr1A-1"
             target="_blank"
             rel="noreferrer"
           >
+            {/* Img containing figma icon */}
             <img className="footer__list__item__link__logo" src={Figma} alt="" /> Figma
           </a>
         </li>
